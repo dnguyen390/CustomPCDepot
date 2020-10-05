7ce52ff93fe5b4ceb0faaeb6a18ada1532ad828c
David Nguyen <59039434+dnguyen390@users.noreply.github.com>
2020-10-05 14:04:20 -0700

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -1,15 +1,27 @@
 <?php
-
-if (isset ($_POST ['submit'])) {
     $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
+    $vistor_email = $_POST['email'];
     $message = $_POST['message'];
 
-$mailto = "dnguyen390@gmail.com";
-$headers = "From: ".$mailFrom;
-$txt = "You have recieved an email from ".$name".\n\n".$message;
 
-mail($mailto, $subject, $txt, $headers);
-header("Location: indext.php?mailsend");
-}
+    $email_from = 'dnguyen390@gmail.com';
+
+    $email_subject = "New Form Submission"
+
+    $email_body = "User Name: $name.\n". 
+                    "User Email: $vistor_email.\n". 
+                        "User Message: $message.\n";
+
+
+    $to = "dnguyen390@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers = "Reply-To: $vistor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
+    ?>
