76a793179993d9c8add1afac83f59376949ecce5
David Nguyen <59039434+dnguyen390@users.noreply.github.com>
2020-10-05 17:56:48 -0700

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,41 @@
 <?php
-    $name = $_POST['name'];
-    $vistor_email = $_POST['email'];
-    $message = $_POST['message'];
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
 
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'dnguyen390@gmail.com';
 
-    $email_from = 'dnguyen390@gmail.com';
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
-    $email_subject = "New Form Submission"
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
-    $email_body = "User Name: $name.\n". 
-                    "User Email: $vistor_email.\n". 
-                        "User Message: $message.\n";
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
 
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-    $to = "dnguyen390@gmail.com";
-
-    $headers = "From: $email_from \r\n";
-
-    $headers = "Reply-To: $vistor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: index.html");
-
-
-    ?>
+  echo $contact->send();
+?>
